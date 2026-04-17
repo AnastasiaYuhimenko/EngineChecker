952d32bdef1faf0c0e6ab9cafd5241c0f2477025
Nastya <mail@yuhimenko-anastasia.ru>
2026-04-17 16:10:24 +0300

    Initial Commit

---
+++ EngineChecker/EngineCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngineCheckerApp.swift
+//  EngineChecker
+//
+//  Created by Anastasia on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct EngineCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
